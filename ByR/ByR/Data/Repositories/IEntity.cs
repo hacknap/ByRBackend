f8675a590f41aa555da0fcd872407b46000b77f7
@hacknap <bernomen99@gmail.com>
2020-09-23 15:50:48 -0400

    Added configuration initial

---
+++ ByR/ByR/Data/Repositories/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ByR.Data.Repositories
+{
+    public interface IEntity
+    {
+        string Id { get; set; }
+
+        [Display(Name = "¿Suspendido?")]
+        bool IsDelete { get; set; }
+
+        [Display(Name = "Fecha Registro")]
+        DateTime Register { get; set; }
+    }
+}
---
+++ ByR/ByR/Data/Repositories/IGeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByR.Data.Repositories
+{
+    public interface IGeneric<T> where T : class
+    {
+        IQueryable<T> GetAll();
+
+        Task<T> GetByIdAsync(string id);
+
+        Task CreateAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task<bool> ExistAsync(string id);
+        Task<T> FindByIdAsync(string id);
+    }
+}
