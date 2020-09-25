391f8a8897c2e819d82927e2778fb570738f8bbe
Usuario <Usuario@LAPTOP-C10E6P09>
2020-09-25 18:42:01 -0400

    Create in helpers PageAndsort

---
+++ ByR/ByR/Helpers/PageAndSortRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByR.Helpers
+{
+    public class PageAndSortRequest
+    {
+        public string Column { get; set; }
+        public string Direction { get; set; }
+        public int Page { get; set; }
+        public int SizePage { get; set; }
+        public string Filter { get; set; }
+    }
+}
